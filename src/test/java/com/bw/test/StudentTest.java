1c6e332dc8463ee591799fcd3bb8b9741a9c59c3
dsh <272745521@qq.com>
2019-12-31 16:06:23 +0800

    第一次提交工程

---
+++ src/test/java/com/bw/test/StudentTest.java
@@ -0,0 +1,12 @@
+package com.bw.test;
+
+import org.junit.Test;
+
+public class StudentTest {
+	
+	
+	@Test
+	public void sss() {
+		System.out.println("--------哈哈-----------");
+	}
+}
---
+++ src/test/java/com/bw/test/UserTest.java
@@ -0,0 +1,21 @@
+package com.bw.test;
+
+import org.junit.Test;
+
+public class UserTest {
+	
+	
+	
+	@Test
+	public void show() {
+		System.out.println("---------------哈哈哈哈----------");
+	}
+	
+	@Test
+	public void show1() {
+		System.out.println("---------------和哈----------");
+	}
+	
+	 
+	
+}
